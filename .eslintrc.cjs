d1701313f3fb506f815cd64d2091f09aded15c8b
Tomaszunek <pawlowskitomaszu@gmail.com>
2023-04-14 21:46:28 +0200

    add eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,12 @@ const config = {
     project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "plugin:import/errors",
+  ],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "warn",
